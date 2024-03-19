39fb050b13c1501697cafabb85af95a9070e297d
refat <khondakar00@gmail.com>
2024-03-19 13:55:04 -0700

    added dynamic loading

--- script.js
+++ script.js
@@ -1,24 +1,82 @@
 document.addEventListener("DOMContentLoaded", function () {
   const filterButtons = document.querySelectorAll(".filter_buttons button");
-  const filterableCards = document.querySelectorAll(".filterable_cards .card");
-
-  console.log(filterableCards, filterButtons);
+  const filterableCards = document.querySelector(".filterable_cards");
 
   const filterCards = (e) => {
-    document.querySelector(".active").classList.remove("active");
-    e.target.classList.add("active");
     const targetName = e.target.dataset.name;
 
-    Array.from(filterableCards).forEach((card) => {
-      card.classList.add("hide");
+    filterButtons.forEach((button) => button.classList.remove("active"));
+    e.target.classList.add("active");
 
-      if (targetName === "all" || card.dataset.name === targetName) {
-        card.classList.remove("hide");
-      }
-    });
+    filterableCards.innerHTML = "";
+
+    if (targetName === "all") {
+      fetch(`images/all`)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.text();
+        })
+        .then((data) => {
+          const parser = new DOMParser();
+          const htmlDocument = parser.parseFromString(data, "text/html");
+          const links = htmlDocument.querySelectorAll("#files a.icon-image");
+
+          links.forEach((link) => {
+            const imageURL = link.getAttribute("href");
+            const card = document.createElement("div");
+            card.classList.add("card", "tall-card");
+            card.innerHTML = `
+                <img src="${imageURL}" alt="" />
+              `;
+            filterableCards.appendChild(card);
+          });
+        })
+        .catch((error) => {
+          console.error("Error fetching or parsing images:", error);
+        });
+    } else {
+      // Fetch images based on the selected category
+      fetch(`images/${targetName}/`)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.text();
+        })
+        .then((data) => {
+          const parser = new DOMParser();
+          const htmlDocument = parser.parseFromString(data, "text/html");
+          const links = htmlDocument.querySelectorAll("#files a.icon-image");
+
+          links.forEach((link) => {
+            const imageURL = link.getAttribute("href");
+            const card = document.createElement("div");
+            card.classList.add("card");
+            card.innerHTML = `
+                <img src="${imageURL}" alt="${targetName}" />
+                <div class="card_body">
+                <h6 class="card_title">${
+                  targetName === "cards"
+                    ? "Gift Card"
+                    : targetName.charAt(0).toUpperCase() + targetName.slice(1)
+                }</h6>
+                <p class="card_text">Lorem ipsum dolor sit amet</p>
+              </div>
+              `;
+            filterableCards.appendChild(card);
+          });
+        })
+        .catch((error) => {
+          console.error("Error fetching or parsing images:", error);
+        });
+    }
   };
 
-  Array.from(filterButtons).forEach((button) => {
+  filterButtons.forEach((button) => {
     button.addEventListener("click", filterCards);
   });
+
+  filterButtons[0].click();
 });
